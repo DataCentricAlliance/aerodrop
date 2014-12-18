160def90c247e2b8db55c77fe13a3beee2abb1cd
Nikita Kuznetsov <me@daedalus.ru>
2014-12-19 01:17:56 +0800

    #test: remove test for get index (obsoleted)

--- test.sh
+++ test.sh
@@ -25,9 +25,6 @@ http -v GET localhost:8080/20141217/query/disk/demo/?user_id_${ITEM_ID}=$ITEM_ID
 echo "MULTIFETCH BY PK"
 http -v GET localhost:8080/20141217/item/disk/demo/1,2,3,4
 
-echo "TEST INDEX"
-http -v GET localhost:8080/20141217/index/disk/demo/index_$ITEM_ID
-
 echo "CLEAN ITEM"
 http -v DELETE localhost:8080/20141217/item/disk/demo/$ITEM_ID
 
