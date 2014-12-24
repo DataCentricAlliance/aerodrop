16ac14bf2c8f5e0dff298a01ce4fc4766137b612
Mikhail Cherepnev <m.cherepnev@facetz.net>
2014-12-24 14:01:45 +0300

    fixed test.sh

--- test.sh
+++ test.sh
@@ -12,12 +12,12 @@ curl -X DELETE localhost:8080/20141217/item/$NAMESPACE/demo/$ITEM_ID
 echo "CREATE INDEX user_id_${ITEM_ID}"
 curl -X PUT localhost:8080/20141217/index/$NAMESPACE/demo/index_$ITEM_ID \
      -H "Content-Type: application/json" \
-     -d '{"key": "user_id_${ITEM_ID}", "type": "STRING"}'
+     -d '{"key": "user_id_'$ITEM_ID'", "type": "STRING"}'
 
 echo "CREATE ITEM"
 curl -X PUT localhost:8080/20141217/item/$NAMESPACE/demo/$ITEM_ID \
      -H "Content-Type: application/json" \
-     -d '{"meta": {"ttl": 3600}, "bins": {"user_id_$ITEM_ID": "$ITEM_ID"}}'
+     -d '{"meta": {"ttl": 3600}, "bins": {"user_id_'$ITEM_ID'": "'$ITEM_ID'"}}'
 
 
 sleep 1
